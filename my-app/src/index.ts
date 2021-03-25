e32dd4619f09ac48cdf52a8b5b23829495482b95
SHIMIZU Taku <shimizu.taku@gmail.com>
2021-03-25 16:45:41 +0900

    Format with prettier

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,23 +1,29 @@
-import { Action, Context, LineContext, withProps } from 'bottender';
+import { Action, Context, LineContext, withProps } from "bottender";
 import { router, route, text } from "bottender/router";
 
-type LineAction = (context: LineContext) => Promise<Action<LineContext> | void>
+type LineAction = (context: LineContext) => Promise<Action<LineContext> | void>;
 
-type LineActionWithProps = (context: LineContext, props: any) => Promise<Action<LineContext> | void>
+type LineActionWithProps = (
+  context: LineContext,
+  props: any
+) => Promise<Action<LineContext> | void>;
 
 const SayHi: LineAction = async (context: LineContext) => {
-  await context.sendText('Hi!');
-}
+  await context.sendText("Hi!");
+};
 
-const SayHiWithName: LineActionWithProps = async (context: LineContext, props: any) => {
-  await context.sendText(`Hi, ${props.name}`)
-}
+const SayHiWithName: LineActionWithProps = async (
+  context: LineContext,
+  props: any
+) => {
+  await context.sendText(`Hi, ${props.name}`);
+};
 
 const Echo: LineAction = async (context: LineContext) => {
   if (context.event.isText) {
     await context.sendText(context.event.text);
   }
-}
+};
 
 const EventCount: LineAction = async (context: LineContext) => {
   const count: number = Number(context.state.count) + 1;
@@ -25,18 +31,18 @@ const EventCount: LineAction = async (context: LineContext) => {
     count,
   });
   await context.sendText(`Count: ${count}`);
-}
+};
 
 const Unknown: LineAction = async (context: LineContext) => {
-  await context.sendText('Sorry.');
-}
+  await context.sendText("Sorry.");
+};
 
 const App: LineAction = async (context: LineContext) => {
   return router([
     text(/^(hi|hello)$/i, SayHi),
-    text('count', EventCount),
-    route('*', Unknown),
+    text("count", EventCount),
+    route("*", Unknown),
   ]);
-}
+};
 
 export default App;
